50c69398c2a22d58910c7a6cefd5848366f94338
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2009-11-30 11:48:52 -0600

    Adding ssh prompt. It shows the host name when connected via ssh.

---
+++ lib/ssh.zsh
@@ -0,0 +1,10 @@
+# Variables for themeing the ssh prompt
+ZSH_THEME_SSH_HOST_NAME_SUFFIX=":"
+
+# Get the host name when connected via ssh
+function ssh_info() {
+  if [[ -n $SSH_TTY ]]; then
+    echo "%m"
+  fi
+  return
+}
