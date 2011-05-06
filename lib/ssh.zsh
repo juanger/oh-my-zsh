10fc9f811ad1ed308a8070addf1952de092d8c40
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2011-05-06 13:36:57 -0500

    New ssh prompt variables.

--- lib/ssh.zsh
+++ lib/ssh.zsh
@@ -1,10 +1,7 @@
-# Variables for themeing the ssh prompt
-ZSH_THEME_SSH_HOST_NAME_SUFFIX=":"
-
 # Get the host name when connected via ssh
 function ssh_info() {
   if [[ -n $SSH_TTY ]]; then
-    echo "%m$ZSH_THEME_SSH_HOST_NAME_SUFFIX"
+    echo "$ZSH_THEME_SSH_PROMPT_PREFIX%m$ZSH_THEME_SSH_PROMPT_SUFFIX"
   fi
   return
 }
